82ed028a37f53c10c4d61ace62d5077a17bbd866
seanmcquaid <sean.mcquaid2@verizon.net>
2019-03-03 21:55:21 -0500

    learned about declaring int variables

---
+++ Section2/HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello there!");
+
+//        assigns a number variable
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int difference = 1000 - myTotal;
+
+        System.out.println(difference);
+    }
+}
